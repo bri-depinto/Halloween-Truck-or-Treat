42860f6e986ee3ecbf8ee965e595ffc926b3b904
bri-depinto <144465570+bri-depinto@users.noreply.github.com>
2024-10-15 01:31:19 +0000

    finished showcase page

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
         $('.pawword').fadeOut();
         $('.pawpic').css('opacity', '100%');
     });
+// end of first pic
 
     $('.candypic').on('mouseenter',() =>{
         $('.candyword').fadeIn();
@@ -17,4 +18,49 @@ $(document).ready(function () {
         $('.candyword').fadeOut();
         $('.candypic').css('opacity', '100%');
     });
+// end of second pic
+
+$('.mariopic').on('mouseenter',() =>{
+    $('.marioword').fadeIn();
+    $('.mariopic').css('opacity', '75%',);
+});
+$('.mariopic').on('mouseleave',() =>{
+    $('.marioword').fadeOut();
+    $('.mariopic').css('opacity', '100%');
+});
+// end of third pic
+
+
+$('.alicepic').on('mouseenter',() =>{
+    $('.aliceword').fadeIn();
+    $('.alicepic').css('opacity', '75%',);
+});
+$('.alicepic').on('mouseleave',() =>{
+    $('.aliceword').fadeOut();
+    $('.alicepic').css('opacity', '100%');
+});
+// end of fourth pic
+
+$('.hawaiipic').on('mouseenter',() =>{
+    $('.hawaiiword').fadeIn();
+    $('.hawaiipic').css('opacity', '75%',);
+});
+$('.hawaiipic').on('mouseleave',() =>{
+    $('.hawaiiword').fadeOut();
+    $('.hawaiipic').css('opacity', '100%');
+});
+// end of fifth pic
+
+
+$('.carnivalpic').on('mouseenter',() =>{
+    $('.carnivalword').fadeIn();
+    $('.carnivalpic').css('opacity', '75%',);
+});
+$('.carnivalpic').on('mouseleave',() =>{
+    $('.carnivalword').fadeOut();
+    $('.carnivalpic').css('opacity', '100%');
+});
+// end of sixth pic
+
+
 });
