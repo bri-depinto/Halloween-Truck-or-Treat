bee6416e6690a3bdb98f57087eee9a874d63dd4a
bri-depinto <144465570+bri-depinto@users.noreply.github.com>
2024-10-15 01:12:12 +0000

    next image on showcase page done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     $('.pawpic').on('mouseenter',() =>{
         $('.pawword').fadeIn();
         $('.pawpic').css('opacity', '75%',);
@@ -6,5 +7,14 @@ $(document).ready(function () {
     $('.pawpic').on('mouseleave',() =>{
         $('.pawword').fadeOut();
         $('.pawpic').css('opacity', '100%');
+    });
+
+    $('.candypic').on('mouseenter',() =>{
+        $('.candyword').fadeIn();
+        $('.candypic').css('opacity', '75%',);
+    });
+    $('.candypic').on('mouseleave',() =>{
+        $('.candyword').fadeOut();
+        $('.candypic').css('opacity', '100%');
     });
 });
